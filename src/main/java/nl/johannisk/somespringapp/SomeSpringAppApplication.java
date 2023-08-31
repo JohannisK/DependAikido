5e7634296df1c0323c8a66994903482671729b3d
Johan Kragt <johankragt@Johans-MacBook-Pro.local>
2023-08-31 12:23:16 +0200

    initial commit

---
+++ src/main/java/nl/johannisk/somespringapp/SomeSpringAppApplication.java
@@ -0,0 +1,13 @@
+package nl.johannisk.somespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeSpringAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SomeSpringAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/nl/johannisk/somespringapp/SomeSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.johannisk.somespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeSpringAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
